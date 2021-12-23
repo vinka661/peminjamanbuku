35991a6fd2ee6919bc79dad0703a856c66f48130
VinkaAmaliaSafitri <Vinka661@gmail.com>
2021-12-23 13:26:16 +0700

    Mengubah Koneksi

--- php/src/koneksi.php
+++ php/src/koneksi.php
@@ -1,7 +1,7 @@
 <?php 
-    $server = "localhost";
+    $server = "172.20.0.2";
     $user = "root";
-    $password = "";
+    $password = "admin";
     $nama_database = "dbperpus";
 
     $db = mysqli_connect( $server, $user, $password, $nama_database);
